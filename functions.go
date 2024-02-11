1ab49a2eae216ee12323e40fea85a5f4662554c4
blackestwhite <pesaregoal@gmail.com>
2024-02-11 16:41:55 +0330

    Refactor: Use constants for result codes and error messages in `RequestResult` and `VerifyResult` functions

--- functions.go
+++ functions.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func PostToZibal(path string, requestData interface{}) (PaymentResponse, error) {
@@ -37,44 +38,29 @@ func PostToZibal(path string, requestData interface{}) (PaymentResponse, error) 
 }
 
 func RequestResult(result string) string {
-	switch result {
-	case "100":
-		return "با موفقیت تایید شد."
-	case "102":
-		return "merchant یافت نشد."
-	case "103":
-		return "Mamerchant غیرفعالrch"
-	case "104":
-		return "merchant نامعتبر"
-	case "201":
-		return "قبلا تایید شده."
-	case "105":
-		return "amount بایستی بزرگتر از 1,000 ریال باشد."
-	case "106":
-		return "callbackUrl نامعتبر می‌باشد. (شروع با http و یا https)"
-	case "113":
-		return "amount مبلغ تراکنش از سقف میزان تراکنش بیشتر است."
-
+	resultCode, err := strconv.Atoi(result)
+	if err != nil {
+		return "خطا در پرداخت"
 	}
-	return "خطا در پرداخت"
+
+	message, found := ResultMessages[resultCode]
+	if !found {
+		return "خطا در پرداخت"
+	}
+
+	return message
 }
 
 func VerifyResult(result string) string {
-	switch result {
-	case "100":
-		return "با موفقیت تایید شد."
-	case "102":
-		return "merchant یافت نشد."
-	case "103":
-		return "merchant غیر فعال"
-	case "104":
-		return "merchant نامعتبر"
-	case "201":
-		return "قبلا تایید شده."
-	case "202":
-		return "سفارش پرداخت نشده یا ناموفق بوده است."
-	case "203":
-		return "trackId نامعتبر می‌باشد."
+	resultCode, err := strconv.Atoi(result)
+	if err != nil {
+		return "خطا در تایید پرداخت"
 	}
-	return "خطا در تایید پرداخت"
+
+	message, found := ResultMessages[resultCode]
+	if !found {
+		return "خطا در تایید پرداخت"
+	}
+
+	return message
 }
