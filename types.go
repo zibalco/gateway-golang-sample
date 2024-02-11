30d11b4342077482852d26e482fb8cd211607b0b
blackestwhite <pesaregoal@gmail.com>
2024-02-11 16:40:16 +0330

    Enhancement: Define constants for result codes and error messages

--- types.go
+++ types.go
@@ -1,5 +1,31 @@
 package main
 
+const (
+	// Result codes
+	SuccessCode          = 100
+	MerchantNotFoundCode = 102
+	MerchantInactiveCode = 103
+	MerchantInvalidCode  = 104
+	AlreadyVerifiedCode  = 201
+	InvalidAmountCode    = 105
+	InvalidCallbackCode  = 106
+	AmountExceedsLimit   = 113
+)
+
+var (
+	// Error messages
+	ResultMessages = map[int]string{
+		SuccessCode:          "با موفقیت تایید شد.",
+		MerchantNotFoundCode: "merchant یافت نشد.",
+		MerchantInactiveCode: "merchant غیر فعال",
+		MerchantInvalidCode:  "merchant نامعتبر",
+		AlreadyVerifiedCode:  "قبلا تایید شده.",
+		InvalidAmountCode:    "amount بایستی بزرگتر از 1,000 ریال باشد.",
+		InvalidCallbackCode:  "callbackUrl نامعتبر می‌باشد. (شروع با http و یا https)",
+		AmountExceedsLimit:   "amount مبلغ تراکنش از سقف میزان تراکنش بیشتر است.",
+	}
+)
+
 type PaymentRequest struct {
 	Merchant    string `json:"merchant"`
 	CallbackURL string `json:"callbackUrl"`
